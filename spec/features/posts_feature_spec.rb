2b7cacf7c788e2952e4a6450b6f4eb27b9c3f251
shelleyhope <shelleyandkenny@mac.com>
2014-05-28 18:07:13 +0100

    users can sign up and in

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -7,38 +7,43 @@ describe 'posts index page' do
       expect(page).not_to have_field 'Title'  
     end
   end
-  # context 'there are no posts to begin with' do
-  #   context 'logged in' do
-  #   	it 'displays a welcome message' do
+  context 'there are no posts to begin with' do
+    context 'logged in' do
+    	it 'displays a welcome message' do
     
-  #     	visit '/posts'
-  #     	expect(page).to have_content "Welcome to Instaham!"
-  # 		end
-  #   end
-  # end	
+      	visit '/posts'
+      	expect(page).to have_content "Welcome to Instaham!"
+  		end
+    end
+  end	
 end
 
 describe 'adding a post' do
   context 'logged out' do
     it "takes us to the sign up page" do
       visit '/posts/new'
-      click_button 'Add your picture or video'
 
-      expect(page).to have_content 'Sign Up'
+      expect(page).to have_content 'Sign up'
       
     end
 
   end
 
-  # context 'logged in' do
-  #   it 'adds a post to the index page' do
-  #   	visit '/posts/new'
-  #   	fill_in 'Title', with: 'Shifie'
-  #   	fill_in 'Description', with: "yet another shite selfie"
-  #     click_button 'Add your picture or video'
+  context 'logged in' do
+    it 'adds a post to the index page' do
+      visit '/users/sign_up'
+      fill_in 'Email', with: 'abc@c.com'
+      fill_in 'Password', with: '12345678'
+      fill_in 'Password confirmation', with: '12345678'
+      click_button 'Sign up'
 
-  #   	expect(current_path).to eq '/posts'
-  #   	expect(page).to have_content 'Shifie'	
-  #   end
-  # end
+      visit '/posts/new'
+    	fill_in 'Title', with: 'Shifie'
+    	fill_in 'Description', with: "yet another shite selfie"
+      click_button 'Add your picture or video'
+
+    	expect(current_path).to eq '/posts'
+    	expect(page).to have_content 'Shifie'	
+    end
+  end
 end
