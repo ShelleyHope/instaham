acbed244a9342dabb5403ce800efbe4ce6906b59
shelleyhope <shelleyandkenny@mac.com>
2014-05-29 15:32:10 +0100

    now using S3 to store image files

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ group :development, :test do
 end
 
 gem 'paperclip', github: 'thoughtbot/paperclip'
+gem 'aws-sdk'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ActiveRecord::Base
-  has_attached_file :picture
+  has_attached_file :picture, 
+  styles: { medium: '300x300>'},
+  storage: :s3,
+  s3_credentials: {
+  	bucket: 'instaham',
+  	access_key_id: Rails.application.secrets.s3_access_key,
+  	secret_access_key: Rails.application.secrets.s3_secret_key
+  }
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 end
